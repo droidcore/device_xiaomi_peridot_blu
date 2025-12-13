d81feca5b6fa76502a27c9cb5d79b268558e982c
droidcore <agusmahardika99@gmail.com>
2025-12-13 12:09:29 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -10,7 +10,7 @@ echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
 git clone -b lineage-23.0 --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
 rm -rf kernel/xiaomi/sm8635-modules
-git clone -b lineage-23.0 --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
+git clone -b lineage-23.1 --depth 1 https://github.com/sm8635-dev/kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
 rm -rf kernel/xiaomi/sm8635-devicetrees
 git clone -b lineage-23.0 --depth 1 https://github.com/droidcore/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
@@ -54,6 +54,18 @@ git clone https://github.com/droidcore/packages_apps_Settings.git packages/apps/
 echo "Cloning Custom system sepolicy tree..."
 rm -rf system/sepolicy
 git clone https://github.com/droidcore/system_sepolicy.git system/sepolicy
+
+echo "Fetching kernel tree..."
+cd kernel/xiaomi/sm8635
+git fetch https://github.com/droidcore/android_kernel_xiaomi_sm8635.git lineage-23.0
+git reset --hard beb37e0cabdd249215e338cba4b7b398f25021b3
+croot
+
+echo "Fetching QPR1 compat..."
+cd kernel/xiaomi/sm8635-devicetrees
+git fetch https://github.com/droidcore/android_kernel_xiaomi_sm8635-devicetrees.git lineage-23.0
+git reset --hard e903a7600fdbb1ebd64256a0ebfafab55e853faf
+croot
 
 # Refresh signing keys
 if [ -d vendor/lineage-priv/keys ]; then
